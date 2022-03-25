38fbaa9365388e3e1336aa32596379c6b4cd498c
ddmrc <63403841+ddmrc@users.noreply.github.com>
2022-03-25 22:57:53 +0100

    Update ft_solver.c

--- C Exercises/BSQ/ft_solver.c
+++ C Exercises/BSQ/ft_solver.c
@@ -16,8 +16,8 @@
 
 int		ft_power(int nb, int power);
 int		ft_sqrt(int nb);
-void	ft_print_map(char **map, int line_col[2], char signs[3]);
-void	ft_print_map_ints(int **map, int line_col[2]);
+void		ft_print_map(char **map, int line_col[2], char signs[3]);
+void		ft_print_map_ints(int **map, int line_col[2]);
 int		**ft_allocat_int_map(int line_col[2]);
 int		ft_fill_copy(int line_col[2], t_b_maps *b_maps);
 int		*ft_find_max_hor_address(int line_col[2], t_b_maps *b_maps, int max);
